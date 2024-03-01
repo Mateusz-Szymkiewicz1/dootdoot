cb3cbd5ed9e01267839da569d10a303a660d85a7
Mateusz <szymkiewiczmateusz1@gmail.com>
2024-03-01 12:13:31 +0100

    dots idk

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+var c = document.querySelector('canvas')
+
 let ilosc_kropek = window.prompt("Podaj liczbę kropek:","");
 let dots = [];
-for(let i = 0;i<ilosc_kropek;i++){
-    dots[i] = Math.floor(Math.random() * (1000)) + 1;
+while(dots.length < ilosc_kropek){
+    let obj = { 
+        x: Math.floor(Math.random() * (c.width)) + 1,
+        y: Math.floor(Math.random() * (c.height)) + 1
+    }
+    if(dots.find(({ x,y }) => x == obj.x && y == obj.y)){
+        continue;
+    }
+    dots[dots.length] = obj; 
 }
+
+var ctx = c.getContext("2d");
+
+let a = dots[Math.floor(Math.random() * dots.length)];
+let b = dots[Math.floor(Math.random() * dots.length)];
+a.a = true;
+b.b = true;
+
+dots.forEach(el => {
+    ctx.strokeStyle = "Purple";
+    console.log(el)
+    if(el.a || el.b){
+        ctx.strokeStyle = "Green";
+    }
+    ctx.moveTo(el.x, el.y);
+    ctx.lineTo(el.x, el.y+1);
+    ctx.stroke();
+})
